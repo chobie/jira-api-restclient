726a05c763511455094f11e2ae4b1992c541ae44
Artjom Kurapov <artkurapov@gmail.com>
2014-12-11 12:14:46 +0200

    Update edit_issue.php

--- examples/edit_issue.php
+++ examples/edit_issue.php
@@ -2,8 +2,20 @@
 require dirname(__FILE__) ."/common.php";
 
 $api = getApiClient();
+
+$updObj = new stdClass();
+$updObj->customfield_10401 = [
+    ['set' => 'Value here']
+];
+
+$r = $api->editIssue($key, [
+    "update" => $updObj
+]);
+
+/*        
 $api->editIssue($key, array(
     "fields" => array(
         "<FieldID>" => "Value"
     ),
 ));
+*/
